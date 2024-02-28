d104d36ec7bf67b18f51142d4739739284853ae6
Hoàng Vũ Nghị <hoangvunghi2004@gmail.com>
2024-02-28 17:15:17 +0700

    update

--- timesheet/views.py
+++ timesheet/views.py
@@ -4,14 +4,14 @@ from rest_framework import status
 from base.models import Employee
 from .models import TimeSheet
 from .serializers import TimeSheetWithUserAccountSerializer, UserAccountWithTimeSheetSerializer,TimeSheetSerializer
-from base.permissions import IsAdminOrReadOnly, IsOwnerOrReadonly
+from base.permissions import IsAdminOrReadOnly,IsOwnerOrReadonly
 from rest_framework import permissions
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator,EmptyPage
 from django.utils import timezone
 from datetime import timedelta
-import platform
-import subprocess
 
+
+#đã test, có thể tìm theo tên
 @api_view(["GET"])
 @permission_classes([IsAdminOrReadOnly])
 def list_timesheet(request):
@@ -65,6 +65,54 @@ def list_timesheet(request):
     }, status=status.HTTP_200_OK)
 
 
+
+# @api_view(['DELETE'])
+# @permission_classes([permissions.IsAuthenticatedOrReadOnly, IsAdminOrReadOnly])
+# def delete_timesheet(request, pk):
+#     try:
+#         timesheet = TimeSheet.objects.get(TimeID=pk)
+#     except TimeSheet.DoesNotExist:
+#         return Response({"error": "Time sheet not found",
+#                          "status":status.HTTP_404_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'DELETE':
+#         if timesheet.TimeID is not None:
+#             timesheet.delete()
+#             return Response({"message": "Time sheet deleted successfully",
+#                              "status":status.HTTP_200_OK}, status=status.HTTP_200_OK)
+#         else:
+#             return Response({"error": "Invalid TimeID",
+#                              "status":status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# # Đã test 
+# @api_view(['POST'])
+# @permission_classes([permissions.IsAuthenticatedOrReadOnly])
+# def create_timesheet(request):
+#     request.data['EmpID'] = request.user.EmpID.EmpID
+#     serializer = TimeSheetSerializer(data=request.data)
+#     required_fields = ['EmpID',"TimeIn","TimeOut"]
+#     emp_id = request.data.get('EmpID', None)
+#     if emp_id != None:
+#         try:
+#             employee = Employee.objects.get(EmpID=emp_id)
+#         except Employee.DoesNotExist:
+#             return Response({"error": f"Employee with EmpID does not exist.",
+#                             "status": status.HTTP_400_BAD_REQUEST},
+#                             status=status.HTTP_400_BAD_REQUEST)
+#     for field in required_fields:
+#         if not request.data.get(field):
+#             return Response({"error": f"{field.capitalize()} is required","status":status.HTTP_400_BAD_REQUEST},
+#                             status=status.HTTP_400_BAD_REQUEST)
+#     if serializer.is_valid():
+#         serializer.validated_data["EmpID"] = request.user.EmpID
+#         serializer.save()
+#         return Response({"message": "TimeSheet created successfully","data":serializer.data,
+#                          "status":status.HTTP_201_CREATED}, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors,
+#                     status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(["GET"])
 @permission_classes([IsOwnerOrReadonly])
 def list_timesheet_nv(request):
@@ -112,100 +160,41 @@ def list_timesheet_nv(request):
 def get_existing_timesheet(emp_id, date):
     return TimeSheet.objects.filter(EmpID=emp_id, TimeIn__date=date).first()
 
-
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticatedOrReadOnly])
 def check_in(request):
     emp_id = request.user.EmpID
     current_date = timezone.now().date()
-    user_agent = request.META.get('HTTP_USER_AGENT', '')
-    print(user_agent)
-    print("------------")
-    
-    serial_number = get_serial_number(user_agent)
-
-    if not serial_number:
-        return Response({"error": "Không thể lấy số serial"}, status=status.HTTP_400_BAD_REQUEST)
-
-    existing_timesheet_with_serial = TimeSheet.objects.filter(serial_number=serial_number, TimeIn__date=current_date).first()
-
-    if existing_timesheet_with_serial:
-        return Response({"error": "Số serial này đã được sử dụng bởi người dùng khác trong ngày hôm nay"}, status=status.HTTP_400_BAD_REQUEST)
 
     existing_timesheet = get_existing_timesheet(emp_id, current_date)
 
-    if existing_timesheet:
-        if existing_timesheet.serial_number == serial_number:
-            return Response({"error": "Bạn đã check-in với số serial này trước đó trong ngày hôm nay"}, status=status.HTTP_400_BAD_REQUEST)
-        
-    checkin_time = timezone.now() + timedelta(hours=7)
     if not existing_timesheet:
-        timesheet = TimeSheet.objects.create(EmpID=emp_id, TimeIn=checkin_time, serial_number=serial_number)
+        timesheet = TimeSheet.objects.create(EmpID=emp_id, TimeIn=timezone.now())
     else:
+        checkin_time = timezone.now()  + timedelta(hours=7)  
         existing_timesheet.data_dict.setdefault("checkin", []).append(checkin_time.strftime("%Y-%m-%d %H:%M:%S"))
-        existing_timesheet.serial_number = serial_number
         existing_timesheet.save()
         timesheet = existing_timesheet
 
     serializer = TimeSheetSerializer(timesheet)
     return Response({"message": "Checked in successfully", "data": serializer.data, "status": status.HTTP_200_OK})
 
-
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticatedOrReadOnly])
 def check_out(request):
     emp_id = request.user.EmpID
     current_date = timezone.now().date()
-    user_agent = request.META.get('HTTP_USER_AGENT', '')
-    serial_number = get_serial_number(user_agent)
-    if not serial_number:
-        return Response({"error": "Không thể lấy số serial"}, status=status.HTTP_400_BAD_REQUEST)
 
     existing_timesheet = get_existing_timesheet(emp_id, current_date)
 
     if not existing_timesheet or not existing_timesheet.TimeIn:
         return Response({"error": "Cannot check out. Not checked in today.", "status": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
 
-    checkout_time = timezone.now()
-    checkout_time_data = timezone.now() + timedelta(hours=7)
+    checkout_time = timezone.now() 
+    checkout_time_data=  timezone.now() + timedelta(hours=7)  
     existing_timesheet.TimeOut = checkout_time
     existing_timesheet.data_dict.setdefault("checkout", []).append(checkout_time_data.strftime("%Y-%m-%d %H:%M:%S"))
-    existing_timesheet.serial_number = serial_number  
     existing_timesheet.save()
 
     serializer = TimeSheetSerializer(existing_timesheet)
     return Response({"message": "Checked out successfully", "data": serializer.data, "status": status.HTTP_200_OK})
-
-
-def get_serial_number(system):
-    if system == 'Windows':
-        try:
-            output = subprocess.check_output(['wmic', 'diskdrive', 'get', 'serialnumber']).decode().strip()
-            serial_number = output.split('\n')[1].strip()
-            return serial_number
-        except Exception as e:
-            print(f"Error: {e}")
-            return None
-    elif system == 'Linux':
-        try:
-            output = subprocess.check_output(['cat', '/proc/cpuinfo']).decode().strip()
-            for line in output.split('\n'):
-                if 'Serial' in line:
-                    serial_number = line.split(':')[-1].strip()
-                    return serial_number
-        except Exception as e:
-            print(f"Error: {e}")
-            return None
-    elif system == 'Darwin':
-        try:
-            output = subprocess.check_output(['system_profiler', 'SPHardwareDataType']).decode().strip()
-            for line in output.split('\n'):
-                if 'Serial Number' in line:
-                    serial_number = line.split(':')[-1].strip()
-                    return serial_number
-        except Exception as e:
-            print(f"Error: {e}")
-            return None
-    else:
-        print("Unsupported operating system.")
-        return None
